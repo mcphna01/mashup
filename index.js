ad2716a1c193bd76c58faec97f7ad118582aa0b6
Nathan James McPhetres <mcphna01@luther.edu>
2018-04-17 00:06:37 -0500

    somewhat final project

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
 "use strict"
 
-//search for youtube videos related to the search parameter
-var search=document.getElementById('search').value
-
-function clickedonyt(){
+function clickedon(){
     "use strict"
+    //set search parameter
+    let search=document.getElementById('search').value
+
+    //clear current links
     let x=document.getElementById('vidlinks')
     x.innerHTML=''
-    let search=document.getElementById('search').value
+    let y=document.getElementById('redlinks')
+    y.innerHTML=''
+
 
     //convert string to query string
     let searchstring=convert(search)
 
-    //get data from youtube api
-    fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&q='
-    	.concat(searchstring).concat('&type=video&key=AIzaSyBo_WaUt2mFH61G2f_nAyP0lQQkS6p3L6Q'))
+    //get data from reddit api
+    fetch('https://www.reddit.com/api/subreddits_by_topic.json?query='.concat(searchstring))
     .then((res)=>res.json())
     .then((data)=>{
-    	//console.log(data)
-    	for (let i=0; i<3; i++){
-    		addvidtopage(data.items[i].id.videoId, data.items[i].snippet.title)
-    	}
+        console.log(data)
+        for (let i=0;i<data.length;i++){
+            addredditlinktopage(data[i].name, data[i].path)
+        }
+    })
+    //get data from youtube api
+    fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&q='.concat(searchstring).concat('&type=video&key=AIzaSyBo_WaUt2mFH61G2f_nAyP0lQQkS6p3L6Q'))
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data)
+        for (let i=0; i<3; i++){
+            addvidtopage(data.items[i].id.videoId, 
+                data.items[i].snippet.title, 
+                data.items[i].snippet.thumbnails.default.url)
+        }
     })
 
 }
 
-function clickedonwa(){
-    "use strict"
-    let search=document.getElementById('search').value
-
-    //convert string to query string
-    let searchstring=convert(search)
-
-    //get data from youtube api
-    fetch('http://www.wolframalpha.com/api/v1/spoken-json.jsp?appid=H2YH7V-9LTQTV2V9E&input='.concat(searchstring))
-    .then((res)=>res.json())
-    .then((data)=>{
-    	console.log(data)
-    })
-
-}
-
-function addvidtopage(id, title){
+//adds video link and thumbnail
+function addvidtopage(id, title, img){
 	var link='https://www.youtube.com/watch?v='.concat(id)
 	let x=document.getElementById('vidlinks')
 	let nlink=document.createElement("a")
+	let nimg=document.createElement('img')
 	nlink.href=link
 	nlink.target='_blank'
+    nlink.class="col-md-4"
 	nlink.innerHTML=title
+	nimg.src=img
+	x.appendChild(nimg)
 	x.appendChild(nlink)
 	let br=document.createElement("br")
 	x.appendChild(br)
-
 }
 
+//adds reddit link
+function addredditlinktopage(name, path){
+	var link='https://www.reddit.com'.concat(path)
+	let x=document.getElementById('redlinks')
+	let nlink=document.createElement("a")
+	nlink.href=link
+	nlink.target='_blank'
+	nlink.innerHTML=name
+	x.appendChild(nlink)
+	let br=document.createElement("br")
+	x.appendChild(br)
+}
+
+//converts a search param into a query string
 function convert(string){
     "use strict"
     var splitstring=string.split(' ')
@@ -66,6 +81,5 @@ function convert(string){
         }
         return searchstring
     }
-    else {var searchstring=string}
-    return searchstring   
+    else {return string}   
 }
